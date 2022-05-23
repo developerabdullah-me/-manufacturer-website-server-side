6bbf762f6a8f0030ea8525c3d85cecdd71cf3635
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-05-23 23:41:21 +0600

    useparams

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,35 +9,46 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.c4kzw.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run (){
-    try{
-        await client.connect();
-   const ServiceCollection=client.db('Parses_go').collection('Parse')
+async function run() {
+  try {
+    await client.connect();
+    const ServiceCollection = client.db("Parses_go").collection("Parse");
 
-app.get('/service',async(req,res) => {
-    const query={}
-    const cursor=ServiceCollection.find(query)
-    const services=await cursor.toArray()
-    res.send(services)
-  
-})
-    }
-    finally{
+    //    get data
+    app.get("/pareses", async (req, res) => {
+      const query = {};
+      const cursor = ServiceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-    }
-    
+    app.get('/purchaseProduct/:id', async (req, res) => {
+      const id =req.params.id;
+      const query = {_id: ObjectId(id)}
+      const result = await ServiceCollection.findOne(query);
+      res.send(result);
+    })
+    // post data
+    app.post("/pareses", async (req, res) => {
+      const newServices = req.body;
+      const result = await ServiceCollection.insertOne(newServices);
+      res.send(result);
+    });
+  } finally {
+  }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 app.get("/", (req, res) => {
-    res.send("running parse go");
-  });
-  
-  app.listen(port, () => {
-    console.log("listen on port", port);
-  });
+  res.send("running parse go");
+});
+
+app.listen(port, () => {
+  console.log("listen on port", port);
+});

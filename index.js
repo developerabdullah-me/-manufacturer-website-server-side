890121059d2bc2b50e1e2b5d27f5b9c006b68f45
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-05-24 22:44:04 +0600

    m

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ const port = process.env.PORT || 5000;
 // middelware
 app.use(cors());
 app.use(express.json());
+verifyJWT
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.c4kzw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -20,6 +37,15 @@ async function run() {
   try {
     await client.connect();
     const ServiceCollection = client.db("Parses_go").collection("Parse");
+    const allUsers = client.db('Parses_go').collection('all-Collections')
+    // app.post("/login", async (req, res) => {
+    //   const user = req.body;
+    //   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "1d",
+    //   });
+    //   res.send({ accessToken });
+    // });
+
 
     //    get data
     app.get("/pareses", async (req, res) => {
@@ -41,6 +67,80 @@ async function run() {
       const result = await ServiceCollection.insertOne(newServices);
       res.send(result);
     });
+// delete
+app.get("/pareses/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: id };
+  const result = await ServiceCollection.deleteOne(query);
+  res.send(result);
+});
+
+app.delete("/pareses/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await ServiceCollection.deleteOne(query);
+  res.send(result);
+});
+
+// get myAddedItems data  from database
+
+app.get("/myAddedItems", verifyJWT, async (req, res) => {
+  const decodedEmail = req.decoded.email;
+  const email = req.query.email;
+  if (email === decodedEmail) {
+    console.log(email);
+    const query = { email: email };
+    console.log(query);
+    const cursor = ServiceCollection.find(query);
+    const InventoryItems = await cursor.toArray();
+    console.log(InventoryItems);
+    res.send(InventoryItems);
+  } else {
+  res.status(403).send({ message: "Access denied! Forbidden access" });
+  }
+});
+// post
+app.post("/myAddedItems", async (req, res) => {
+  const newService = req.body;
+  const service = await ServiceCollection.insertOne(newService);
+  res.send(service);
+});
+
+// user
+app.put('/user/:email', async (req, res) => {
+  const email = req.params.email
+  // console.log(email);
+  const user = req.body
+  // console.log(user);
+  const filter = { email: email }
+  // console.log(filter);
+  const options = { upsert: true }
+  // console.log(options);
+  const updatedDoc = {
+      $set: user,
+  }
+  // console.log(updatedDoc);
+  const result = await allUsers.updateOne(filter, updatedDoc, options)
+  const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2d' })
+  res.send({result,token});
+})
+
+
+// put for updatedQuantity and restack
+app.put('/purchaseProduct/:id', async (req, res) => {
+  const id = req.params.id
+  const updatedQuantity = req.body.updatedData
+  const filter = { _id: ObjectId(id )}
+  const options = { upsert: true }
+  const updatedDoc = {
+      $set: {
+          quantity: updatedQuantity
+      }
+  }
+  const result = await ServiceCollection.updateOne(filter, updatedDoc, options)
+  res.send(result)
+})
+
   } finally {
   }
 }
